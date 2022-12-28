6f9d42bbfa82e8cd3bdfb6dce573a5784c711ad4
Rob Hamburg <robhamburg89@gmail.com>
2022-12-28 01:35:20 +0000

    style header

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let sideList = document.querySelector(".sidebar ul")
 let dash = document.querySelector(".mdi-view-dashboard")
 let sideSpan = document.querySelectorAll(".sidebar ul li span")
 let listItems = document.querySelectorAll(".sidebar ul li")
-
 let x = window.matchMedia("(max-width: 700px)")
 
 dash.addEventListener('click', myFunction)
@@ -16,7 +15,7 @@ function myFunction() {
     if (x.matches) { 
         side.classList.toggle('invisible');
 sideList.setAttribute('style', 'flex-flow: row; justify-content: space-evenly; height: max-content;')
-
+dash.textContent = ""
 
         for (let i = 0; i < sideSpan.length; i++) {
             sideSpan[i].textContent = ""
